a2029ebc313c6f5ad94708357a36e3e436acf6de
Jodu555 <Jodu505@gmail.com>
2021-12-12 15:24:23 +0100

    moved the logic to a seperate script file

---
+++ clock/script.js
@@ -0,0 +1,50 @@
+const clock = document.querySelector('#clock');
+let previousType = '';
+setInterval(updateClock, 1000);
+
+function getCurrentTimeType() {
+    return document.querySelector('#time')?.classList.contains('active')
+        ? 'Time'
+        : document.querySelector('#date').classList.contains('active')
+            ? 'Date'
+            : '';
+}
+
+function updateClock() {
+    const type = getCurrentTimeType();
+    previousType = type;
+    const fn = 'toLocale' + type + 'String';
+    const time = new Date()[fn]();
+    clock.innerHTML = time;
+}
+
+particlesJS.load('particles', 'particles.json', () => {
+    console.log('callback - particles.js config loaded');
+});
+
+[...document.querySelectorAll('button:not(.active)')].forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const type = getCurrentTimeType();
+        if (btn.id !== type.toLowerCase()) {
+            console.log(123);
+            clock.style.opacity = '0%';
+            setTimeout(() => {
+                clock.style.opacity = '100%';
+                updateClock();
+            }, 350);
+        }
+    });
+});
+
+[...document.querySelectorAll('.button-group button')].forEach((btn) => {
+    btn.addEventListener('click', () => {
+        clearButtons();
+        btn.classList.add('active');
+    });
+});
+
+function clearButtons() {
+    [...document.querySelectorAll('button.active')].forEach((btn) => {
+        btn.classList.remove('active');
+    });
+}
