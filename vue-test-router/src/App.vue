3ad9f9bbd8512345d4e4aa5b70ca3bb841744d8a
Jodu555 <Jodu505@gmail.com>
2021-12-26 00:44:23 +0100

    Tried the router transitions

--- vue-test-router/src/App.vue
+++ vue-test-router/src/App.vue
@@ -1,7 +1,11 @@
 <template>
 	<div>
 		<Navbar />
-		<router-view class="container py-5" />
+		<router-view v-slot="{ Component }">
+			<transition name="slide">
+				<component class="container py-5" :is="Component" />
+			</transition>
+		</router-view>
 	</div>
 </template>
 
@@ -14,3 +18,35 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss">
+.wrapper {
+	width: 100%;
+	min-height: 100vh;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+	transition: all 0.75s ease-out;
+}
+
+.slide-enter-to {
+	position: absolute;
+	right: 0;
+}
+
+.slide-enter-from {
+	position: absolute;
+	right: -100%;
+}
+
+.slide-leave-to {
+	position: absolute;
+	left: -100%;
+}
+
+.slide-leave-from {
+	position: absolute;
+	left: 0;
+}
+</style>
