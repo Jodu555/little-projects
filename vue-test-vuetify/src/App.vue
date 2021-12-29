2bbbf0ebb696a07edecfeb18a23faab021378472
Jodu555 <Jodu505@gmail.com>
2021-12-29 01:33:42 +0100

    Added a default cuetify app also for testing purposes with especially dark mode

---
+++ vue-test-vuetify/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-app>
+		<v-main>
+			<v-theme-provider root>
+				<router-view />
+			</v-theme-provider>
+		</v-main>
+	</v-app>
+</template>
+
+<script>
+export default {
+	name: 'App',
+
+	data: () => ({
+		//
+	}),
+};
+</script>
---
+++ vue-test-vuetify/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<v-bottom-navigation>
+			<v-btn value="recent">
+				<span>Recent</span>
+
+				<v-icon>mdi-history</v-icon>
+			</v-btn>
+
+			<v-btn value="favorites">
+				<span>Favorites</span>
+
+				<v-icon>mdi-heart</v-icon>
+			</v-btn>
+
+			<v-btn value="nearby">
+				<span>Nearby</span>
+
+				<v-icon>mdi-map-marker</v-icon>
+			</v-btn>
+		</v-bottom-navigation>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+
+	components: {},
+};
+</script>
