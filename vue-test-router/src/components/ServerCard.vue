a6cd10813e27d6fd34280151db3289fcac8ad150
Jodu555 <Jodu505@gmail.com>
2022-02-25 00:06:50 +0100

    extracted into the server card with the assignable props

---
+++ vue-test-router/src/components/ServerCard.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="card card-hover shadow-lg p-3 mb-5 rounded">
+		<div class="card-body">
+			<h5 class="card-title">
+				<h4>
+					<b>{{ name }}</b>
+				</h4>
+			</h5>
+			<div class="justify-content-centermt-4">
+				<div class="row">
+					<div class="col card-text" style="font-size: 1.5rem">
+						<div class="row mt-2">
+							<dt class="col-sm-7">(v)Cores</dt>
+							<dd class="col-sm-5">{{ cores }}</dd>
+						</div>
+						<div class="row mt-2">
+							<dt class="col-sm-7">Memory</dt>
+							<dd class="col-sm-5">{{ memory }}GB</dd>
+						</div>
+						<div class="row mt-2">
+							<dt class="col-sm-7">Storage</dt>
+							<dd class="col-sm-5">{{ storage }}GB</dd>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="float-end mt-3">
+				<a href="#" class="btn btn-outline-primary">Buy</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['name', 'cores', 'memory', 'storage'],
+};
+</script>
+
+<style scoped>
+.card-hover {
+	transition: all 0.5s ease-in-out;
+	border-radius: 1.25rem !important;
+}
+
+.card-hover:hover {
+	box-shadow: 0px 0px 20px 5px #141313ed !important;
+	transform: scale(1.1) translateY(20px);
+}
+</style>
--- vue-test-router/src/views/About.vue
+++ vue-test-router/src/views/About.vue
@@ -3,49 +3,22 @@
 		<h1>This is an about page</h1>
 		<div class="row">
 			<div class="col">
-				<div class="card card-hover shadow-lg p-3 mb-5 rounded">
-					<div class="card-body">
-						<h5 class="card-title">
-							<h4><b>Silver</b></h4>
-						</h5>
-						<div class="justify-content-centermt-4">
-							<div class="row">
-								<div class="col card-text" style="font-size: 1.5rem">
-									<div class="row mt-2">
-										<dt class="col-sm-7">(v)Cores</dt>
-										<dd class="col-sm-4">2</dd>
-									</div>
-									<div class="row mt-2">
-										<dt class="col-sm-7">Memory</dt>
-										<dd class="col-sm-4">4GB</dd>
-									</div>
-									<div class="row mt-2">
-										<dt class="col-sm-7">Storage</dt>
-										<dd class="col-sm-4">5GB</dd>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="float-end mt-3">
-							<a href="#" class="btn btn-outline-primary">Buy</a>
-						</div>
-					</div>
-				</div>
+				<ServerCard name="Bronze" cores="1" memory="1" storage="5"></ServerCard>
 			</div>
-			<div class="col"></div>
+			<div class="col">
+				<ServerCard name="Silver" cores="2" memory="4" storage="10"></ServerCard>
+			</div>
 			<div class="col"></div>
 			<div class="col"></div>
 		</div>
 	</div>
 </template>
 
-<style scoped>
-.card-hover {
-	transition: all 0.4s ease-in-out;
-}
-
-.card-hover:hover {
-	box-shadow: 0px 0px 20px 5px #141313ed !important;
-	transform: scale(1.1) translateY(30px);
-}
-</style>
+<script>
+import ServerCard from '@/components/ServerCard';
+export default {
+	components: {
+		ServerCard,
+	},
+};
+</script>
