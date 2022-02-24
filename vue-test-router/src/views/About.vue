80808d5c803e749c2272f53210c0dacae71531c3
Jodu555 <Jodu505@gmail.com>
2022-02-24 23:49:52 +0100

    added some thing like later server cards

--- vue-test-router/src/views/About.vue
+++ vue-test-router/src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<div class="row">
+			<div class="col">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">
+							<h4><b>Silver</b></h4>
+						</h5>
+						<div class="justify-content-centermt-4">
+							<div class="row">
+								<div class="col card-text" style="font-size: 1.5rem">
+									<div class="row mt-2">
+										<dt class="col-sm-7">(v)Cores</dt>
+										<dd class="col-sm-4">2</dd>
+									</div>
+									<div class="row mt-2">
+										<dt class="col-sm-7">Memory</dt>
+										<dd class="col-sm-4">4GB</dd>
+									</div>
+									<div class="row mt-2">
+										<dt class="col-sm-7">Storage</dt>
+										<dd class="col-sm-4">5GB</dd>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="float-end mt-3">
+							<a href="#" class="btn btn-outline-primary">Buy</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col"></div>
+			<div class="col"></div>
+			<div class="col"></div>
+		</div>
+	</div>
 </template>
