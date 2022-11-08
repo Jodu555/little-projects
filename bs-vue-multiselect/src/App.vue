c4d0f4927241f69c4583950ca2afd492d5abb0a2
Jodu555 <Jodu505@gmail.com>
2022-11-08 20:13:17 +0100

    added a basic navbar + the list but without special logic for sortable

--- bs-vue-multiselect/src/App.vue
+++ bs-vue-multiselect/src/App.vue
@@ -1,12 +1,34 @@
 <template>
 	<div>
 		<header>
-			<!-- place navbar here -->
+			<nav class="navbar navbar-expand-sm navbar-light bg-light">
+				<div class="container">
+					<p class="navbar-brand">My Test Project for all my silly Ideas</p>
+					<button
+						class="navbar-toggler d-lg-none"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapsibleNavId"
+						aria-controls="collapsibleNavId"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="collapsibleNavId">
+						<ul class="me-auto"></ul>
+						<a href="#" class="btn btn-outline-info">Profile</a>
+					</div>
+				</div>
+			</nav>
 		</header>
 
 		<main class="container mt-4">
 			<!-- <MultiSelect :playlists="state.playlists" :checkAll="checkAll" /> -->
 			<!-- next test Sortable List -->
+			<ul class="list-group">
+				<li v-for="list in state.playlists" class="list-group-item">{{ list.name }}</li>
+			</ul>
 		</main>
 
 		<footer>
