bd3b4bfa8c6c4d03e3115d53bae9730fcb3d2ab7
Jodu555 <Jodu505@mail.com>
2022-11-18 18:58:02 +0100

    Showed the todo

--- axios-test/src/App.vue
+++ axios-test/src/App.vue
@@ -1,13 +1,18 @@
 <template>
 	<h1>Hello</h1>
+	<pre>{{ todo }}</pre>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import networking from '@/utils/networking';
+
+const todo = ref({});
+
 onMounted(async () => {
-	const data = await networking.get('todos/1');
-	console.log(data);
+	const response = await networking.get('todos/1');
+	console.log(response);
+	todo.value = response.data;
 });
 </script>
 
