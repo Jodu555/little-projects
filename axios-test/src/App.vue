9ff3be81c57b2108586bb19b816757ce4c0269cb
Jodu555 <Jodu505@mail.com>
2022-11-18 18:48:38 +0100

    Added the basic axios instance usage

--- axios-test/src/App.vue
+++ axios-test/src/App.vue
@@ -1,31 +1,15 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<h1>Hello</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { onMounted } from 'vue';
+import networking from './utils/networking';
+onMounted(async () => {
+	console.log(`the component is now mounted.`);
+	const data = await networking.get('todos/1');
+	console.log(data);
+});
+</script>
+
+<style scoped></style>
