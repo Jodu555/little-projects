87fd3bc2781c4b41fb1a030d999d9d262945d4f1
Jodu555 <Jodu505@gmail.com>
2022-12-14 14:08:36 +0100

    Implemented a native vue way for AutoComplete

--- bs-vue-multiselect/src/components/AutoComplete.vue
+++ bs-vue-multiselect/src/components/AutoComplete.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="dropdown">
 		<input
+			ref="inputRef"
 			@click="click"
 			@input="input"
 			@keydown="keydown"
@@ -11,19 +12,76 @@
 			autocomplete="off"
 			data-bs-toggle="dropdown"
 		/>
-		<div ref="dropdownMenu" class="dropdown-menu"></div>
+		<div ref="dropdownMenuRef" v-show="recommendations.length >= 1" class="dropdown-menu" style="margin: 0px">
+			<button v-for="recommendation in recommendations" type="button" class="dropdown-item">
+				<span v-for="value in recommendation.values" :class="{ 'text-primary': value.h }">
+					{{ value.value }}
+				</span>
+			</button>
+		</div>
 	</div>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref, reactive } from 'vue';
 import { Dropdown } from 'bootstrap';
 
-const dropdownMenu = ref(null);
-const dropdown = new Dropdown(dropdownMenu);
-function click() {}
+const data = reactive(['Hello', 'Hallo', 'Tasta', 'Test', 'Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet']);
 
-function input() {}
+const recommendations = ref([]);
 
-function keydown() {}
+const inputRef = ref(null);
+const dropdownMenuRef = ref(null);
+let dropdown;
+onMounted(() => {
+	dropdown = new Dropdown(dropdownMenuRef.value);
+});
+
+function click(e) {}
+
+function input() {
+	dropdown.show();
+	const maximumItems = 5;
+	const lookup = inputRef.value.value.toLowerCase();
+	if (lookup.trim() === '') {
+		recommendations.value = [];
+		return;
+	}
+
+	recommendations.value = data
+		.map((key) => {
+			const idx = removeDiacritics(key).toLowerCase().indexOf(removeDiacritics(lookup).toLowerCase());
+			if (idx >= 0) {
+				return {
+					taken: true,
+					values: [
+						{ value: key.substring(0, idx), h: false },
+						{ value: key.substring(idx, idx + lookup.length), h: true },
+						{ value: key.substring(idx + lookup.length, key.length), h: false },
+					],
+				};
+			} else {
+				return {
+					taken: false,
+				};
+			}
+		})
+		.filter((x) => x.taken);
+	recommendations.value.splice(maximumItems, recommendations.value.length);
+}
+
+function keydown(e) {
+	if (e.keyCode === 27) {
+		dropdown.hide();
+		return;
+	}
+	if (e.keyCode === 40) {
+		dropdown._menu.children[0]?.focus({ preventScroll: true });
+		return;
+	}
+}
+
+function removeDiacritics(str) {
+	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
 </script>
 <style lang=""></style>
