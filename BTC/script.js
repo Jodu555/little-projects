e117d53e0b021ee80f041bc56767d3e446a400f8
Jodu555 <Jodu505@gmail.com>
2023-02-07 16:40:27 +0100

    Added Celo as another for me interesting currencie!

--- BTC/script.js
+++ BTC/script.js
@@ -1,34 +1,36 @@
-const btc = document.querySelector('#btc')
-const xmr = document.querySelector('#xmr')
+const btc = document.querySelector('#btc');
+const xmr = document.querySelector('#xmr');
+const celo = document.querySelector('#celo');
 
 async function loadData() {
-    Promise.all([
-        loadBTC(),
-        loadXMR()
-    ])
-
+	Promise.all([loadBTC(), loadXMR(), loadCelo()]);
 }
 
-async function loadXMR() {
-    const response = await fetch('https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=BTC,USD,EUR')
-    const json = await response.json();
+async function loadCelo() {
+	const response = await fetch('https://mkt-api.kminfra.net/api/web-token/celo');
+	const json = await response.json();
 
-    xmr.innerHTML = `XMR: ${json.EUR} €`
+	celo.innerHTML = `Celo: ${json.data.celo.price} €`;
+}
+async function loadXMR() {
+	const response = await fetch('https://mkt-api.kminfra.net/api/web-token/xmr');
+	const json = await response.json();
+
+	xmr.innerHTML = `XMR: ${json.data.xmr.price} €`;
 }
 async function loadBTC() {
-    const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-    const json = await response.json();
+	const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+	const json = await response.json();
 
-    var floatPrice = json.bpi.EUR.rate;
-    var euro = floatPrice.split('.')[0];
-    var cleanEuro = euro.replace(',', '.');
+	var floatPrice = json.bpi.EUR.rate;
+	var euro = floatPrice.split('.')[0];
+	var cleanEuro = euro.replace(',', '.');
 
-    btc.innerHTML = `BTC: ${cleanEuro} ${json.bpi.EUR.symbol}`;
+	btc.innerHTML = `BTC: ${cleanEuro} ${json.bpi.EUR.symbol}`;
 }
 
 setInterval(() => {
-    loadData();
+	loadData();
 }, 10000);
-
 
 loadData();
